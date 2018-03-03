b3ea11a8f96bf2c13212ff4ab0b6913f7da5e0da
spdev <spdevuk@gmail.com>
2018-03-03 14:01:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+//components
+
+//css
+import './styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hey
+      </div>
+    );
+  }
+}
+
+export default App;
