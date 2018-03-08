db15e4b4a5e4583f9304453b267075bb5be644d9
spdev <spdevuk@gmail.com>
2018-03-08 18:27:46 +0000

    Added styling for contact button and added placeholder icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,7 +10,7 @@ class Navbar extends Component {
     super();
   }
 
-  burgerToggle() {
+burgerToggle() {
     let linksEl = document.querySelector('.navbar-mobile-links');
     if (linksEl.style.display === 'block') {
       linksEl.style.display = 'none';
@@ -22,21 +22,24 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
+      <div className="navbar-icon">
+        <img src={TradingPostIcon} alt="Trading Post Icon"/>
+      </div>
         <div className="navbar-wide">
-          <div className="navbar-wide-div">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-            <a href="#">Link 4</a>
+          <div className="navbar-wide-links">
+            <a className="navbar-wide-links-active" href="#">Ebay</a>
+            <a className="navbar-wide-links-active" href="#">Craigslist</a>
+            <a className="navbar-wide-links-active" href="#">OfferUp</a>
+            <a className="navbar-wide-links-active contact-link" href="#">Contact</a>
           </div>
         </div>
         <div className="navbar-mobile">
           <i onClick={this.burgerToggle} className="fa fa-bars fa-2x navbar-mobile-burger" />
           <div className="navbar-mobile-links">
-            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 1</a>
-            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 2</a>
-            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 3</a>
-            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 4</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Ebay</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Craigslist</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">OfferUp</a>
+            <a className="navbar-mobile-links-active contact-link"  href="https://www.google.com">Contact</a>
           </div>
         </div>
       </nav>
