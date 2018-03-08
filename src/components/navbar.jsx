a676623948ec064cb5838b952b01b5060cbe2124
spdev <spdevuk@gmail.com>
2018-03-08 17:43:20 +0000

    Changed navbar stuff

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import classNames from 'classnames';
 
 import TradingPostIcon from '../images/tradingpost.svg';
-//css
+// css
 import '../styles/navbar.css';
-
-
 
 class Navbar extends Component {
   constructor() {
-    super()
-    this.mobileMenu = this.mobileMenu.bind(this);
+    super();
   }
 
-  mobileMenu() {
-    document.getElementByClassName("")
+  burgerToggle() {
+    let linksEl = document.querySelector('.navbar-mobile-links');
+    if (linksEl.style.display === 'block') {
+      linksEl.style.display = 'none';
+    } else {
+      linksEl.style.display = 'block';
+    }
   }
 
   render() {
     return (
       <nav className="navbar">
-      <div className="nav-toggle">
-        <i onClick={mobileMenu}className="fa fa-bars" aria-hidden="true"></i>
-      </div>
-        <ul>
-          <div className="navbar-icon">
-            <li><a href="https://www.google.com"><img src={TradingPostIcon}/></a></li>
+        <div className="navbar-wide">
+          <div className="navbar-wide-div">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+            <a href="#">Link 4</a>
           </div>
-          <li><a className="navbar-link" href="https://www.google.com">Craigslist</a></li>
-          <li><a className="navbar-link" href="https://www.google.com">Offerup</a></li>
-          <li><a className="navbar-link" href="https://www.google.com">Ebay</a></li>
-          <li><a className="navbar-link" href="https://www.google.com">Contact</a></li>
-        </ul>
+        </div>
+        <div className="navbar-mobile">
+          <i onClick={this.burgerToggle} className="fa fa-bars fa-2x navbar-mobile-burger" />
+          <div className="navbar-mobile-links">
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 1</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 2</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 3</a>
+            <a className="navbar-mobile-links-active"  href="https://www.google.com">Link 4</a>
+          </div>
+        </div>
       </nav>
     );
   }
