7b04f58866777f1b1fde40d1d923a91110c0778e
spdev <spdevuk@gmail.com>
2018-03-11 15:37:06 +0000

    set up dates and map areas, finisheds dates+times

--- src/components/contactinfo.jsx
+++ src/components/contactinfo.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import '../styles/footer.css';
+import '../styles/contactinfo.css';
 
 const ContactInfo = () => {
   return (
     <div className="contactinfo">
-      hey
+      <div className="contactinfo-times">
+        <div className="contactinfo-times-title">
+          <p>Opening Times</p>
+        </div>
+        <p className="contactinfo-times-day">Monday 10:00 - 19:00</p>
+        <p className="contactinfo-times-day">Tuesday  10:00 - 19:00</p>
+        <p className="contactinfo-times-day">Wednesday 10:00 - 19:00</p>
+        <p className="contactinfo-times-day">Thursday 10:00 - 19:00</p>
+        <p className="contactinfo-times-day">Friday 10:00 - 19:00</p>
+        <p className="contactinfo-times-day">Saturday 10:00 - 18:00</p>
+        <p className="contactinfo-times-day">Sunday 10:00 - 16:00</p>
+      </div>
+      <div className="contactinfo-location">
+        <div className="contactinfo-location-title">
+          <p>Location</p>
+          <p>7100 Teckler Blvd</p>
+        </div>
+        <div className="contactinfo-location-map">
+          map
+        </div>
+      </div>
     </div>
   );
 };
