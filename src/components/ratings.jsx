b2855b78536b7c788fc8661f7b3d9d496c97e027
spdev <spdevuk@gmail.com>
2018-03-18 01:52:53 +0000

    added photo image slider & set it up for all resolutions & responsiveness etc

--- src/components/ratings.jsx
+++ src/components/ratings.jsx
@@ -4,8 +4,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/ratings.css';
 
-//set autoplay to false until updating react-slick, currently causes an error when on autoplay and you unmount the component
-
 const Ratings = () => {
   const settings = {
     dots: true,
@@ -22,16 +20,14 @@ const Ratings = () => {
       <div className="ratings">
         <div className="ratings-reviews">
           <Slider {...settings}>
-            <div id="ebay" className="ratings-reviews-review">
-              <div className="ratings-reviews-review-icon ratings-reviews-review-icon-ebay">
-                <i className="fa fa-thumbs-up" />
-              </div>
+            <div className="ratings-reviews-review">
+              <div className="ratings-reviews-review-icon ratings-reviews-review-icon-facebook"><i className="fa fa-thumbs-up" /></div>
               <div className="ratings-reviews-review-title">
-                <p>Ebay</p>
+                <p>Facebook</p>
               </div>
               <div className="ratings-reviews-review-stars">
-                <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" />
-                <p>5 Stars - 6286 Ratings</p>
+                <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star-half" />
+                <p>4.5 Stars</p>
               </div>
             </div>
             <div className="ratings-reviews-review">
@@ -46,14 +42,16 @@ const Ratings = () => {
                 <p>5 Stars - 79 Ratings</p>
               </div>
             </div>
-            <div className="ratings-reviews-review">
-              <div className="ratings-reviews-review-icon ratings-reviews-review-icon-facebook"><i className="fa fa-thumbs-up" /></div>
+            <div id="ebay" className="ratings-reviews-review">
+              <div className="ratings-reviews-review-icon ratings-reviews-review-icon-ebay">
+                <i className="fa fa-thumbs-up" />
+              </div>
               <div className="ratings-reviews-review-title">
-                <p>Facebook</p>
+                <p>Ebay</p>
               </div>
               <div className="ratings-reviews-review-stars">
-                <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star-half" />
-                <p>4.5 Stars</p>
+                <i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" />
+                <p>5 Stars - 6286 Ratings</p>
               </div>
             </div>
           </Slider>
--- src/pages/album/album.jsx
+++ src/pages/album/album.jsx
@@ -1,21 +1,105 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import Slider from 'react-slick';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './album.css';
+//images
+import img01 from '../../images/01.jpg';
+import img02 from '../../images/02.jpg';
+import img03 from '../../images/03.jpg';
+import img04 from '../../images/04.jpg';
+import img05 from '../../images/05.jpg';
+import img06 from '../../images/06.jpg';
+import img07 from '../../images/07.jpg';
+import img08 from '../../images/08.jpg';
+import img10 from '../../images/10.jpg';
+import img11 from '../../images/11.jpg';
+import img12 from '../../images/12.jpg';
+import img13 from '../../images/13.jpg';
+import img14 from '../../images/14.jpg';
 
-const Album = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="album">
-        <div className="contact-hero">
+export default class Album extends Component {
+  render() {
+    const settingsAlbum = {
+      dots: true,
+      className: 'center',
+      arrows: true,
+      centerMode: true,
+      infinite: true,
+      autoplay: true,
+      speed: 400,
+      lazyload: true,
+      autoplayspeed: 3500,
+      slidesToShow: 3,
+      responsive: [{
+        breakpoint: 1201,
+        settings: {
+          lazyload: true,
+          centerMode: false,
+          arrows:false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }],
+    };
+    return (
+      <div >
+        <Navbar/>
+        <div className="album">
+          <div className="album-container">
+          <Slider {...settingsAlbum}>
+            <div className="album-container-images">
+              <img src={img01} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img02} alt=" Electronics"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img03} alt=" Jewlery"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img04} alt=" Signed Jersey"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img05} alt="Laptops and Speakers"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img06} alt="Home Electric Appliances"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img07} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img08} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img10} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img11} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img12} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img13} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            <div className="album-container-images">
+              <img src={img14} alt="Trading Post Crystal Lake Pets"/>
+            </div>
+            </Slider>
+          </div>
+          <div className="container">
+          hello
+          </div>
         </div>
+        <Footer/>
       </div>
-      <Footer />
-    </div>
-  );
-};
-
-export default Album;
+    );
+  }
+}
